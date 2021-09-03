2165bebfd153d2b60e058baaa76e8b8299c4a656
ya0guang <hbchen001@gmail.com>
2021-09-03 11:41:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ToyPythonCompiler
 For IU Compiler course
+
+## Resources
+
+The framework of this repo is from [IUCompilerCourse/python-student-support-code](https://github.com/IUCompilerCourse/python-student-support-code).
+
+- [Course website](https://iucompilercourse.github.io/IU-Fall-2021/)
+- [Racket Version Book](https://www.dropbox.com/s/ktdw8j0adcc44r0/book.pdf?dl=1)
+- [Python Version Book](https://www.dropbox.com/s/mfxtojk4yif3toj/python-book.pdf?dl=1)
+- [Python `ast` doc](https://docs.python.org/3.10/library/ast.html#ast.parse)
+
+## Environment
+
+I use `pyenv` to manage Python environment. Since `match` clause is just introduced in Python 3.10, which is still an `rc` version and not stable yet, it can be installed using `pyenv` and related packages can also be managed in a clean fashion.
+
+## python-student-support-code
+
+Support code for students (Python version).
+
+The runtime.c file needs to be compiled by doing the following
+
+```sh
+   gcc -c -g -std=c99 runtime.c
+```
+This will produce a file named runtime.o. The -g flag is to tell the compiler to produce debug information that you may need to use the gdb (or lldb) debugger.
---
+++ compiler.py
@@ -0,0 +1,93 @@
+import ast
+from ast import *
+from utils import *
+from x86_ast import *
+import os
+from typing import List, Tuple, Set, Dict
+
+Binding = Tuple[Name, expr]
+Temporaries = List[Binding]
+
+
+class Compiler:
+
+    ############################################################################
+    # Remove Complex Operands
+    ############################################################################
+
+    def rco_exp(self, e: expr, need_atomic: bool) -> Tuple[expr, Temporaries]:
+        # YOUR CODE HERE
+        pass
+
+    def rco_stmt(self, s: stmt) -> List[stmt]:
+        # YOUR CODE HERE
+        pass
+        
+
+    def remove_complex_operands(self, p: Module) -> Module:
+        # YOUR CODE HERE
+        pass
+        
+
+    ############################################################################
+    # Select Instructions
+    ############################################################################
+
+    def select_arg(self, e: expr) -> arg:
+        # YOUR CODE HERE
+        pass        
+
+    def select_stmt(self, s: stmt) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def select_instructions(self, p: Module) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Assign Homes
+    ############################################################################
+
+    def assign_homes_arg(self, a: arg, home: Dict[Variable, arg]) -> arg:
+        # YOUR CODE HERE
+        pass        
+
+    def assign_homes_instr(self, i: instr,
+                           home: Dict[location, arg]) -> instr:
+        # YOUR CODE HERE
+        pass        
+
+    def assign_homes_instrs(self, ss: List[instr],
+                            home: Dict[location, arg]) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def assign_homes(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Patch Instructions
+    ############################################################################
+
+    def patch_instr(self, i: instr) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def patch_instrs(self, ss: List[instr]) -> List[instr]:
+        # YOUR CODE HERE
+        pass        
+
+    def patch_instructions(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
+    ############################################################################
+    # Prelude & Conclusion
+    ############################################################################
+
+    def prelude_and_conclusion(self, p: X86Program) -> X86Program:
+        # YOUR CODE HERE
+        pass        
+
