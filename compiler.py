3a9f5ebfb97589266d1308e2a70956aab4580bb9
ya0guang <hbchen001@gmail.com>
2021-09-04 16:43:42 -0400

    Using Name doesn't work in select

--- compiler.py
+++ compiler.py
@@ -17,7 +17,7 @@ class Compiler:
     ############################################################################
 
     def rco_exp(self, e: expr, need_atomic: bool) -> Tuple[expr, Temporaries]:
-        
+
         def is_atm(e: expr):
             """helper function to check if `e` is an `atm` """
             match e:
@@ -26,7 +26,7 @@ class Compiler:
                 case Name(_):
                     return True
             return False
-        
+
         temps = []
         # tail must be assigned in the match cases
         if is_atm(e):
@@ -45,25 +45,24 @@ class Compiler:
                     (exp1_atm, exp1_temps) = (exp1, [])
                 else:
                     (exp1_atm, exp1_temps) = self.rco_exp(exp1, True)
-                
+
                 if is_atm(exp2):
                     (exp2_atm, exp2_temps) = (exp2, [])
                 else:
                     (exp2_atm, exp2_temps) = self.rco_exp(exp2, True)
-                
+
                 tail = BinOp(exp1_atm, Add(), exp2_atm)
                 temps = exp1_temps + exp2_temps
             case Call(Name('input_int'), []):
-                return (e, temps)
-        
+                tail = e
+
         if need_atomic:
             var = Name("pyc_temp_var_" + str(self.temp_count))
             temps.append((var, tail))
             self.temp_count += 1
             tail = var
-        
-        return (tail, temps)
 
+        return (tail, temps)
 
     def rco_stmt(self, s: stmt) -> List[stmt]:
         result = []
@@ -81,33 +80,77 @@ class Compiler:
 
         for binding in temps:
             result.append(Assign([binding[0]], binding[1]))
-        
+
         result.append(tail)
         return result
-        
 
     def remove_complex_operands(self, p: Module) -> Module:
         match p:
             case Module(stmts):
-                new_stmts_list = [self.rco_stmt(s) for s in stmts]
-                new_stmts = [s for stmts in new_stmts_list for s in stmts]
+                new_stmts = [new_stat for s in stmts for new_stat in self.rco_stmt(s)]
                 return Module(new_stmts)
 
     ############################################################################
     # Select Instructions
     ############################################################################
-
+    
     def select_arg(self, e: expr) -> arg:
-        # YOUR CODE HERE
-        pass        
+        match e:
+            case Constant(c):
+                return Immediate(c)
+            case Name(var):
+                return e
+
+    def select_expr(self, e: expr) -> List[instr]:
+        instrs = []
+        match e:
+            case Call(Name('input_int'), []):
+                instrs.append(Callq("read_int", 0))
+                instrs.append(Instr('movq', [Reg('rax'), Name("Unnamed_Pyc_Var")]))
+            case UnaryOp(USub(), atm):
+                instrs.append(Instr('negq', [Name("Unnamed_Pyc_Var")]))
+            case BinOp(atm1, Add(), atm2):
+                # TODO: optimize when the oprand and destination is the same
+                # if isinstance(atm1, Name):
+                #     (atm1, atm2) = (atm2, atm1)
+                # if isinstance(atm2, Name):
+                #     instrs.append(Instr('addq', [select_arg, Name("Unbounded_Pyc_Var")]))
+                instrs.append(Instr('movq', [self.select_arg(atm1), Name("Unnamed_Pyc_Var")]))
+                instrs.append(Instr('addq', [self.select_arg(atm2), Name("Unnamed_Pyc_Var")]))
+        
+        return instrs
+
 
     def select_stmt(self, s: stmt) -> List[instr]:
-        # YOUR CODE HERE
-        pass        
+
+        def bound_unamed(instrs: List[instr], var: str) -> List[instr]:
+            new_instrs = []
+            for i in instrs:
+                match i:
+                    case Instr(oprtr, args):
+                        new_args = [Name(var) if a == Name("Unnamed_Pyc_Var") else a for a in args]
+                        new_instrs.append(Instr(oprtr, new_args))
+            
+            return new_instrs
+                            
+        instrs = []
+        match s:
+            case Expr(Call(Name('print'), [atm])):
+                instrs.append(Instr('movq', [self.select_arg(atm), Reg('rdi')]))
+                instrs.append(Callq("print_int", 1))
+            case Expr(exp):
+                instrs += self.select_expr(exp)
+            case Assign([Name(var)], exp):
+                instrs += bound_unamed(self.select_expr(exp), var)
+        
+        return instrs
+        
 
     def select_instructions(self, p: Module) -> X86Program:
-        # YOUR CODE HERE
-        pass        
+        match p:
+            case Module(stmts):
+                insts = [inst for s in stmts for inst in self.select_stmt(s)]
+                return X86Program(insts)
 
     ############################################################################
     # Assign Homes
@@ -115,21 +158,21 @@ class Compiler:
 
     def assign_homes_arg(self, a: arg, home: Dict[Variable, arg]) -> arg:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes_instr(self, i: instr,
                            home: Dict[location, arg]) -> instr:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes_instrs(self, ss: List[instr],
                             home: Dict[location, arg]) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
+        pass
 
     ############################################################################
     # Patch Instructions
@@ -137,15 +180,15 @@ class Compiler:
 
     def patch_instr(self, i: instr) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def patch_instrs(self, ss: List[instr]) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def patch_instructions(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
+        pass
 
     ############################################################################
     # Prelude & Conclusion
@@ -153,5 +196,4 @@ class Compiler:
 
     def prelude_and_conclusion(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
-
+        pass
