a2d7fb5990582155e9e6de1963e335721b26f892
llabuzie <llabuzie@iu.edu>
2021-12-12 20:41:17 -0500

    starting to add tests

--- run-tests.py
+++ run-tests.py
@@ -2,10 +2,17 @@ import os
 import compiler
 import interp_Pvar
 import type_check_Pvar
+import interp_Llambda
+import type_check_Llambda
+import interp_Clambda
+import type_check_Clambda
 from utils import run_tests, run_one_test
 
 compiler = compiler.Compiler()
-if False:
+run_tests('lambda', compiler, 'lambda', type_check_Llambda.TypeCheckLlambda().type_check, interp_Llambda.InterpLlambda().interp,type_check_Clambda.TypeCheckClambda().type_check, interp_Clambda.InterpClambda().interp)
+
+
+""" if False:
     run_one_test(os.getcwd() + '/tests/var/zero.py', 'var',
                  compiler, 'var',
                  type_check_Pvar.TypeCheckPvar().type_check_P,
@@ -15,5 +22,5 @@ else:
     run_tests('var', compiler, 'var',
               type_check_Pvar.TypeCheckPvar().type_check_P,
               interp_Pvar.InterpPvar().interp_P,
-              None)
+              None) """
 
